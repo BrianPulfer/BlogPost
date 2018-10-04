98ea8f027cb9da65f48234950bf9d1edecf22603
Brian Pulfer <brian.pulfer@student.supsi.ch>
2018-10-04 23:12:33 +0200

    Modifiche ai metodi doGet() e doPost() della classe BlogPost.

--- src/main/java/ch/supsi/webapp/BlogPost.java
+++ src/main/java/ch/supsi/webapp/BlogPost.java
@@ -1,40 +1,74 @@
 package ch.supsi.webapp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 
-@WebServlet(value="/blogpost")
+@WebServlet(value = "/blogpost")
 
 public class BlogPost extends HttpServlet {
 
     private String author;
     private String text;
     private String title;
+    private ObjectMapper mapper = new ObjectMapper();
+    //ArrayList necessario a contenere tutte le nostre istanze (niente database per ora)
+    private static ArrayList<BlogPost> instances = new ArrayList<>();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Get BlogPost");
+        OutputStream responseStream = resp.getOutputStream();
+
+        for(int i = 0; i<instances.size(); i++){
+            mapper.writeValue(responseStream,instances.get(i));
+        }
+
+        System.out.println(instances); //A scopo di debug...
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Post BlogPost");
+        BlogPost creato = null;
+
+        System.out.println(req.getContentType());
+
+        if(req.getContentType().equals("application/x-www-form-urlencoded")){
+            creato = mapper.readValue(req.getQueryString(),BlogPost.class);
+        }else {
+            StringBuilder body = new StringBuilder();
+            BufferedReader o = req.getReader();
+            String s = new String();
+            while(!( s =  o.readLine()).isEmpty()){
+                body.append(s);
+            }
+
+            if(!body.toString().isEmpty())
+                creato = mapper.readValue(body.toString(),BlogPost.class);//Bug
+        }
+
+        if(creato != null) {
+            instances.add(creato);
+
+            OutputStream respOutputStream = resp.getOutputStream();
+            mapper.writeValue(respOutputStream, creato);
+        }
+
+        System.out.println(instances);      //A scopo di debug...
     }
 
     @Override
     protected void doHead(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //Non ancora implementato. Non richiesto in questo esercizio.
     }
-
-    //ArrayList necessario a contenere tutte le nostre istanze (niente database per ora)
-    private static ArrayList<BlogPost> instances = new ArrayList<>();
-
 
     public BlogPost(){
         addToArrayList();
