e2d7adf2145ed63624c5287022a4367ef932f6b1
Brian Pulfer <brian.pulfer@student.supsi.ch>
2019-03-06 14:14:02 +0100

    Risoluzione di un bug che permetteva di cliccare il bottone nascosto (da console su browser)

--- src/main/resources/static/js/registrationValidation.js
+++ src/main/resources/static/js/registrationValidation.js
@@ -3,35 +3,37 @@ let surname = document.getElementById("surname");
 let username = document.getElementById("username");
 let password1 = document.getElementById("password1");
 let password2 = document.getElementById("password2");
-let button_created = false;
 
-/*
-<div class="form-group">
-                    <button id="registerbutton" type="submit" class="btn btn-primary btn-block">Register</button>
-                </div>
- */
+let button_is_showing = false;
+
 
 function createButton(){
-    let div = document.createElement("DIV");
-    div.class = "form-group"
+    if(!button_is_showing) {
+        let div = document.createElement("DIV");
+        div.class = "form-group"
+        div.id = "createdButton"
 
-    let button = document.createElement("BUTTON");
-    button.id = "registerbutton";
-    button.type = "submit";
-    button.className = "btn btn-primary btn-block";
-    button.innerHTML = "Register";
+        let button = document.createElement("BUTTON");
+        button.id = "registerbutton";
+        button.type = "submit";
+        button.className = "btn btn-primary btn-block";
+        button.innerHTML = "Register";
 
-    div.appendChild(button);;
-    document.getElementById("form").appendChild(div);
+        div.appendChild(button);
+        ;
+        document.getElementById("form").appendChild(div);
+
+        button_is_showing = true;
+    }
 }
 
-function showRegisterButton(){
-    document.getElementById("registerbutton").hidden = false;
+function deleteButton(){
+    if(button_is_showing) {
+        document.getElementById("form").removeChild(document.getElementById("createdButton"));
+        button_is_showing = false;
+    }
 }
 
-function hideRegisterButton(){
-    document.getElementById("registerbutton").hidden = true;
-}
 
 function validPassword(pass, pass2){
     if(/^[a-zA-Z0-9_-_]+$/.test(pass.value) && pass.value == pass2.value && pass.value.length >= 8 && pass.value.length <= 15){
@@ -73,14 +75,9 @@ function validUsername(username){
 
 function checkIfOk(){
     if(validName(name)  && validSurname(surname) && validUsername(username) && validPassword(password1,password2)){
-        if(!button_created){
-            createButton();
-            button_created = true;
-        }
-        showRegisterButton();
+        createButton();
     } else {
-        if(button_created)
-            hideRegisterButton();
+        deleteButton();
     }
 }
 
